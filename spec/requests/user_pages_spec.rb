1c8ad0b0444d0b02b09c3724370b6d84a4907ed2
Sam Blackman <sam.blackm@gmail.com>
2013-03-22 06:13:26 +0000

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,7 +59,7 @@ describe "User pages" do
 				let(:user) {User.find_by_email("example@user.com")}
 				it { should have_selector('title', text: user.name) }
 				it { should have_selector('div.alert.alert-success', text: "Welcome") }
-				it { should have_link('Sign out')}
+				it { should have_link('Sign out' )}
 			end
 		end
 
